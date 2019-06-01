5b004807d0fa56bdfb9a4c137ecafa060f68c875
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-06-01 16:50:52 +0100

    Add service configuration parser
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ cfg/services_cfg.go
@@ -0,0 +1,97 @@
+package cfg
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/goware/urlx"
+)
+
+// Example configuration file
+// ---
+// services:
+//   - name: my-service
+//     upstream_uri: http://my-service.local
+//     matchers:
+//       - host: my-service.mydomain.com
+//       - host: my-svc.mydomain.com
+//     headers:
+//       Authorization: Basic my-basic-auth-secret
+//
+//   - name: my-other-service
+//     upstream_uri: http://my-service.local
+//     matchers:
+//       - host: my-other-service.mydomain.com
+//
+
+// MatcherConfig represents an unvalidated Matcher configuration
+type MatcherConfig struct {
+	Host string
+}
+
+// ValidatedMatcherConfig represents a validated Matcher configuration
+type ValidatedMatcherConfig struct {
+	Host string
+}
+
+// ServiceConfig represents an unvalidated Service configuration
+type ServiceConfig struct {
+	Name        string            `json:"name"`
+	UpstreamURI string            `json:"upstream_uri"`
+	Matchers    []MatcherConfig   `json:"matchers"`
+	Headers     map[string]string `json:"headers"`
+}
+
+// Validate does validation of MatcherConfig
+func (c *MatcherConfig) Validate() (ValidatedMatcherConfig, error) {
+	cfg := ValidatedMatcherConfig{}
+
+	if c.Host == "" {
+		return cfg, fmt.Errorf("Matcher Host cannot be empty")
+	}
+
+	return ValidatedMatcherConfig{
+		Host: c.Host,
+	}, nil
+}
+
+// ValidatedServiceConfig represents a validated Service configuration
+type ValidatedServiceConfig struct {
+	Name        string
+	UpstreamURI url.URL
+	Matchers    []ValidatedMatcherConfig
+	Headers     map[string]string
+}
+
+// Validate does validation of ServiceConfig
+func (c *ServiceConfig) Validate() (ValidatedServiceConfig, error) {
+	cfg := ValidatedServiceConfig{}
+
+	if c.Name == "" {
+		return cfg, fmt.Errorf("Service Name cannot be empty")
+	}
+
+	upstreamURI, err := urlx.ParseWithDefaultScheme(c.UpstreamURI, "https")
+	if err != nil {
+		return cfg, fmt.Errorf("Service Upstream URI must be a valid URI")
+	}
+
+	validatedMatchers := make([]ValidatedMatcherConfig, 0)
+
+	for index, matcher := range c.Matchers {
+		validatedMatcher, err := matcher.Validate()
+		if err != nil {
+			return cfg, fmt.Errorf(
+				"Service Matcher %d was not valid: %s", index, err,
+			)
+		}
+		validatedMatchers = append(validatedMatchers, validatedMatcher)
+	}
+
+	return ValidatedServiceConfig{
+		Name:        c.Name,
+		UpstreamURI: *upstreamURI,
+		Matchers:    validatedMatchers,
+		Headers:     c.Headers,
+	}, nil
+}
---
+++ cfg/services_cfg_test.go
@@ -0,0 +1,164 @@
+package cfg
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Matcher Config", func() {
+	It("Parses a valid configuration", func() {
+		cfg := MatcherConfig{Host: "my-service.mydomain.com"}
+		validatedCfg, err := cfg.Validate()
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(validatedCfg.Host).To(Equal("my-service.mydomain.com"))
+	})
+
+	It("Does not validated a configuration with an empty host", func() {
+		cfg := MatcherConfig{Host: ""}
+		_, err := cfg.Validate()
+
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(MatchError(ContainSubstring(
+			"Matcher Host cannot be empty",
+		)))
+	})
+})
+
+var _ = Describe("Service Config", func() {
+	It("Parses a valid configuration", func() {
+		cfg := ServiceConfig{
+			Name:        "my-service",
+			UpstreamURI: "my-service.local",
+			Matchers: []MatcherConfig{
+				MatcherConfig{Host: "my-service.mydomain.com"},
+				MatcherConfig{Host: "my-svc.mydomain.com"},
+			},
+			Headers: map[string]string{
+				"Authorization": "Basic my-basic-auth",
+			},
+		}
+
+		validatedCfg, err := cfg.Validate()
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(validatedCfg.Name).To(Equal("my-service"))
+		Expect(validatedCfg.Headers).To(Equal(cfg.Headers))
+
+		Expect(validatedCfg.UpstreamURI.String()).To(Equal(
+			"https://my-service.local",
+		))
+
+		Expect(validatedCfg.Matchers).To(HaveLen(2))
+		Expect(validatedCfg.Matchers[0].Host).To(Equal("my-service.mydomain.com"))
+		Expect(validatedCfg.Matchers[1].Host).To(Equal("my-svc.mydomain.com"))
+	})
+
+	It("Parses a valid configuration even if matchers are omitted", func() {
+		cfg := ServiceConfig{
+			Name:        "my-service",
+			UpstreamURI: "my-service.local",
+			Headers: map[string]string{
+				"Authorization": "Basic my-basic-auth",
+			},
+		}
+
+		validatedCfg, err := cfg.Validate()
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(validatedCfg.Name).To(Equal("my-service"))
+		Expect(validatedCfg.Headers).To(Equal(cfg.Headers))
+
+		Expect(validatedCfg.UpstreamURI.String()).To(Equal(
+			"https://my-service.local",
+		))
+
+		Expect(validatedCfg.Matchers).To(HaveLen(0))
+	})
+
+	It("Parses a valid configuration even if headers are omitted", func() {
+		cfg := ServiceConfig{
+			Name:        "my-service",
+			UpstreamURI: "my-service.local",
+			Matchers: []MatcherConfig{
+				MatcherConfig{Host: "my-service.mydomain.com"},
+				MatcherConfig{Host: "my-svc.mydomain.com"},
+			},
+		}
+
+		validatedCfg, err := cfg.Validate()
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(validatedCfg.Name).To(Equal("my-service"))
+
+		Expect(validatedCfg.UpstreamURI.String()).To(Equal(
+			"https://my-service.local",
+		))
+
+		Expect(validatedCfg.Matchers).To(HaveLen(2))
+		Expect(validatedCfg.Matchers[0].Host).To(Equal("my-service.mydomain.com"))
+		Expect(validatedCfg.Matchers[1].Host).To(Equal("my-svc.mydomain.com"))
+	})
+
+	It("Does not validate a configuration without a name", func() {
+		cfg := ServiceConfig{
+			UpstreamURI: "my-service.local",
+			Matchers: []MatcherConfig{
+				MatcherConfig{Host: "my-service.mydomain.com"},
+				MatcherConfig{Host: "my-svc.mydomain.com"},
+			},
+			Headers: map[string]string{
+				"Authorization": "Basic my-basic-auth",
+			},
+		}
+
+		_, err := cfg.Validate()
+
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(MatchError(ContainSubstring(
+			"Service Name cannot be empty",
+		)))
+	})
+
+	It("Does not validate a configuration with an invalid upstream uri", func() {
+		cfg := ServiceConfig{
+			Name:        "my-service",
+			UpstreamURI: "!",
+			Matchers: []MatcherConfig{
+				MatcherConfig{Host: "my-service.mydomain.com"},
+				MatcherConfig{Host: "my-svc.mydomain.com"},
+			},
+			Headers: map[string]string{
+				"Authorization": "Basic my-basic-auth",
+			},
+		}
+
+		_, err := cfg.Validate()
+
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(MatchError(ContainSubstring(
+			"Service Upstream URI must be a valid URI",
+		)))
+	})
+
+	It("Does not validate a configuration with an invalid matcher", func() {
+		cfg := ServiceConfig{
+			Name:        "my-service",
+			UpstreamURI: "my-service.local",
+			Matchers: []MatcherConfig{
+				MatcherConfig{Host: "my-service.mydomain.com"},
+				MatcherConfig{},
+			},
+			Headers: map[string]string{
+				"Authorization": "Basic my-basic-auth",
+			},
+		}
+
+		_, err := cfg.Validate()
+
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(MatchError(ContainSubstring(
+			"Service Matcher 1 was not valid",
+		)))
+	})
+})
